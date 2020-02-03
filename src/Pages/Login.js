2c2361d081c6f365343f2f4e3d9fef870b865f54
frontendgod <3054750147@qq.com>
2020-02-03 12:26:04 +0800

    p27

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import 'antd/dist/antd.css'
+import {Card, Input, Icon, Button, Spin} from 'antd'
+import '../static/css/Login.css'
 
 function Login() {
+
+    const [useName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(()=>{
+            setIsLoading(false)
+        }, 1000)
+    }
+
     return (
-        <div>
-            我是登录页面
+        <div className="login-div">
+
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="JSPang blog System" bordered={true} style={{width:400}}>
+                    <Input 
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<Icon type="user" style={{color:'rgba(0,0,0,.25)'}}/>}
+                        onChange={(e)=>{setUserName(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <Input.Password 
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your password"
+                        prefix={<Icon type="key" style={{color:'rgba(0,0,0,.25)'}}/>}
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                    />
+                    <br/><br/>
+
+                    <Button type="primary" size="large" block onClick={checkLogin}>登录</Button>
+                </Card>
+            </Spin>
+
         </div>
     )
 }
