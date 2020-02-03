f6bcbac92ec828b98eedcfbf64746df55577e0d4
frontendgod <3054750147@qq.com>
2020-02-03 13:38:49 +0800

    p29

---
+++ src/Pages/AddArticle.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import marked from 'marked'
+import '../static/css/AddArticle.css'
+import {Row,Col,Input,Select,Button,DatePicker} from 'antd'
+const {Option} = Select
+const {TextArea} = Input
+
+function AddArticle() {
+    return (
+        <div>
+            <Row gutter={5}>
+                <Col span={18}>
+                    <Row gutter={10}>
+                        <Col span={20}>
+                            <Input
+                                placeholder="博客标题"
+                                size="large"
+                            />
+                        </Col>
+                        <Col span={4}>
+                            &nbsp;
+                            <Select defaultValue="1" size="large">
+                                <option value="1">视频教程</option>
+                            </Select>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row gutter={10}>
+                        <Col span={12}>
+                            <TextArea 
+                                className="markdown-content"
+                                rows={35}
+                                placeholder="文章内容"
+                            />
+                        </Col>
+                        <Col span={12}>
+                            <div className="show-html"></div>
+
+                        </Col>
+
+                    </Row>
+
+                </Col>
+                <Col span={6}></Col>
+            </Row>
+        </div>
+    )
+}
+
+export default AddArticle
--- src/Pages/AdminIndex.js
+++ src/Pages/AdminIndex.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import '../static/css/AdminIndex.css'
+import {Route} from 'react-router-dom'
+import AddArticle from './AddArticle'
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -47,13 +49,18 @@ function AdminIndex() {
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
+
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>后台管理系统</Breadcrumb.Item>
               <Breadcrumb.Item>工作台</Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>JSPANG的博客管理系统</div>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              <div>
+                <Route path="/index/" exact component={AddArticle}></Route>
+              </div>
+              JSPANG的博客管理系统
+            </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
         </Layout>
