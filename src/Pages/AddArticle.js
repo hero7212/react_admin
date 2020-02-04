dc9befd95f68fadcc079f51925d6f0736b7b6335
frontendgod <3054750147@qq.com>
2020-02-04 12:36:25 +0800

    p36

--- src/Pages/AddArticle.js
+++ src/Pages/AddArticle.js
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import marked from 'marked'
 import '../static/css/AddArticle.css'
-import {Row,Col,Input,Select,Button,DatePicker} from 'antd'
+import {Row,Col,Input,Select,Button,DatePicker, message} from 'antd'
 import axios from 'axios'
 import servicePath from '../config/apiUrl'
 const {Option} = Select
@@ -18,7 +18,7 @@ function AddArticle(props) {
     const [showDate,setShowDate] = useState()   //发布日期
     const [updateDate,setUpdateDate] = useState() //修改日志的日期
     const [typeInfo ,setTypeInfo] = useState([]) // 文章类别信息
-    const [selectedType,setSelectType] = useState(1) //选择的文章类别
+    const [selectedType,setSelectType] = useState('请选择') //选择的文章类别
 
     useEffect(()=>{
         getTypeInfo()
@@ -67,6 +67,31 @@ function AddArticle(props) {
     }
 
 
+    const selectTypeHandler = (value) => {
+        setSelectType(value)
+    }
+
+    const saveArticle = () => {
+        if(!selectedType){
+            message.error('必须选择文章类型')
+            return false
+        }else if (!articleTitle) {
+            message.error('文章名称不能为空')
+            return false
+        }else if (!articleContent) {
+            message.error('文章内容不能为空')
+            return false
+        }else if (!introducemd) {
+            message.error('文章简介不能为空')
+            return false
+        }else if (!showDate) {
+            message.error('发布日期不能为空')
+            return false
+        }
+        message.success('检验通过')
+    }
+
+
     return (
         <div>
             <Row gutter={5}>
@@ -74,13 +99,15 @@ function AddArticle(props) {
                     <Row gutter={10}>
                         <Col span={20}>
                             <Input
+                                value={articleTitle}
                                 placeholder="博客标题"
                                 size="large"
+                                onChange={e=>{setArticleTitle(e.target.value)}}
                             />
                         </Col>
                         <Col span={4}>
                             &nbsp;
-                            <Select defaultValue={selectedType} size="large">
+                            <Select defaultValue={selectedType} size="large" onChange={selectTypeHandler}>
                                 {
                                     typeInfo.map((item, index) => {
                                     return (<Option key={index} value={item.id}>{item.typeName}</Option>)
@@ -115,7 +142,7 @@ function AddArticle(props) {
                     <Row>
                         <Col span={24}>
                             <Button size="large">暂存文章</Button>&nbsp;
-                            <Button type="primary" size="large">发布文章</Button>
+                            <Button type="primary" size="large" onClick={saveArticle}>发布文章</Button>
                             <br />
                         </Col>
                         <Col span={24}>
@@ -133,6 +160,7 @@ function AddArticle(props) {
                         <Col span={12}>
                             <div className="date-select">
                                 <DatePicker 
+                                    onChange={(date, dateString)=>{setShowDate(dateString)}}
                                     placeholder="发布日期"
                                     size="large"
                                 />
