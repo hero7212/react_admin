91d433749933aa85271daaa01027712c09459977
frontendgod <3054750147@qq.com>
2020-02-04 18:56:08 +0800

    p39

--- src/Pages/AdminIndex.js
+++ src/Pages/AdminIndex.js
@@ -3,17 +3,26 @@ import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import '../static/css/AdminIndex.css'
 import {Route} from 'react-router-dom'
 import AddArticle from './AddArticle'
+import ArticleList from './ArticleList'
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-function AdminIndex() {
+function AdminIndex(props) {
   
     const [collapsed, setCollapsed] = useState(false)
 
     const onCollapse = collapsed => {
 
         setCollapsed(collapsed)
+    }
+
+    const handleClickArticle = e => {
+      if(e.key=='addArticle') {
+        props.history.push('/index/add')
+      } else {
+        props.history.push('/index/list')
+      }
     }
 
   
@@ -32,6 +41,7 @@ function AdminIndex() {
             </Menu.Item>
             <SubMenu
               key="sub1"
+              onClick={handleClickArticle}
               title={
                 <span>
                   <Icon type="user" />
@@ -39,8 +49,8 @@ function AdminIndex() {
                 </span>
               }
             >
-              <Menu.Item key="3">添加文章</Menu.Item>
-              <Menu.Item key="4">文章列表</Menu.Item>
+              <Menu.Item key="addArticle">添加文章</Menu.Item>
+              <Menu.Item key="articleList">文章列表</Menu.Item>
             </SubMenu>
             <Menu.Item key="9">
               <Icon type="file" />
@@ -58,6 +68,8 @@ function AdminIndex() {
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
               <div>
                 <Route path="/index/" exact component={AddArticle}></Route>
+                <Route path="/index/add/" exact component={AddArticle}></Route>
+                <Route path="/index/list/" exact component={ArticleList}></Route>
               </div>
               JSPANG的博客管理系统
             </div>
